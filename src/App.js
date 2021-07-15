612f9e395979715d8d966c6ac1382c27089d5d8c
rabina <rabinacathrine@gmail.com>
2021-07-15 17:28:54 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+// import Foodbox from './Components/Foodbox';
+// import Search from './Components/Search';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+        
+//       </header> */}
+//       <header className="App-header">
+//         Pro-Nutrition
+//       </header>
+//       <Search/>
+//       <Foodbox/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
+// import Foodbox from './FoodBox/FoodBoxComponent'
+// import Title from './TitileBar/Title'
+import Foodbox from './Components/Foodbox';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<div>
+          {/* <Title /> */}
+          <h1>Pro-Nutrition</h1>
+          <Foodbox />
+  </div>);
 }
 
 export default App;
